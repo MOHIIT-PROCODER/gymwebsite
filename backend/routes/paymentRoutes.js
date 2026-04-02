883b22a9534b46d1d82cee7c4bbc041843c37fb1
MOHIT-650 <mohantasarojkumar65@gmail.com>
2026-04-02 12:18:08 +0530

    fix api url

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,10 +1,4 @@
-// const express = require("express");
-// const router = express.Router();
-// const { createPayment } = require("../controllers/paymentController");
 
-// router.post("/create", createPayment);
-
-// module.exports = router;
 
 
 const express = require("express");
